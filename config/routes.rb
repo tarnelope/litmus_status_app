34a23dd64104213c1365cac4993c262b649f229d
Teresa Tarn <teresa.tarn@kapost.com>
2018-08-07 16:29:18 -0400

    Added StaticPages root, view components, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :status, only: [:create, :update]
+
+      resources :status_messages, only: [:create]
+    end
+  end
 end
