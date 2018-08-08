e5f469f96d75b3ea61d944199c08c1b8dafd8c1f
Teresa Tarn <teresa.tarn@kapost.com>
2018-08-07 21:36:40 -0400

    Updated index view to display the basics

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def index
+    @current_status = current_status
+    @status_messages = status_messages
+  end
+
+  private
+
+  def current_status
+    @_current_status ||= Status.last
+  end
+
+  def status_messages
+    @_status_messages ||= StatusMessage.order(created_at: :desc)
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def status_class(current_status)
+    current_status.current_status == 'UP' ? 'alert-success' : 'alert-danger'
+  end
 end
