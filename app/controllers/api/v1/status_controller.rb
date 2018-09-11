a45e336898d5d6ed7b333abc384847d9661e81ac
Teresa Tarn <teresa.tarn@kapost.com>
2018-09-11 13:36:10 -0400

    Added functionality to optionally create StatusMessage when posting a status
    - added more definition to status_controller_spec

--- app/controllers/api/v1/status_controller.rb
+++ app/controllers/api/v1/status_controller.rb
@@ -4,7 +4,17 @@ module Api
       before_action :authenticate_user!, only: :create
 
       def create
-        @status = Status.new(permitted_params)
+        @status = Status.new(status_permitted_params)
+
+        if params[:description]
+          StatusMessage.create(message_permitted_params)
+          
+          # TODO: Add more defense against bad messages
+          # status_message = StatusMessage.new(message_permitted_params)
+          # unless status_message.save
+          #   render json: @status_message.errors.full_messages, status: 400
+          # end
+        end
 
         if @status.save
           render json: @status, status: 200
@@ -15,8 +25,12 @@ module Api
 
       private
 
-      def permitted_params
+      def status_permitted_params
         params.permit(:current_status)
+      end
+
+      def message_permitted_params
+        params.permit(:description)
       end
     end
   end
--- spec/controllers/api/v1/status_controller_spec.rb
+++ spec/controllers/api/v1/status_controller_spec.rb
@@ -38,21 +38,7 @@ RSpec.describe Api::V1::StatusController, type: :controller do
         end
       end
 
-      context 'with invalid current_status' do
-        let(:current_status) { %w[123 elephant mango].sample }
-
-        it_behaves_like 'an invalid request'
-      end
-
-      context 'with empty current_status' do
-        let(:params) { {} }
-
-        it_behaves_like 'an invalid request'
-      end
-
-      context 'with valid current_status' do
-        let(:current_status) { %w[UP DOWN up down].sample }
-
+      shared_examples_for 'a valid request' do
         it 'returns 200' do
           post!
           expect(response.status).to eql(200)
@@ -60,6 +46,72 @@ RSpec.describe Api::V1::StatusController, type: :controller do
 
         it 'creates new Status' do
           expect { post! }.to change { Status.count }.by(1)
+        end
+      end
+
+      describe 'invalid current_status' do
+        context 'current_status is number string' do
+          let(:current_status) { '123' }
+          it_behaves_like 'an invalid request'
+        end
+
+        context 'current_status is string no equal to up or down' do
+          let(:current_status) { 'elephant' }
+          it_behaves_like 'an invalid request'
+        end
+      end
+
+      context 'empty current_status' do
+        let(:params) { {} }
+        it_behaves_like 'an invalid request'
+      end
+
+      describe 'uppercase valid current_status' do
+        context 'current_status=UP' do
+          let(:current_status) { 'UP' }
+          it_behaves_like 'a valid request'
+        end
+
+        context 'current_status=DOWN' do
+          let(:current_status) { 'DOWN' }
+          it_behaves_like 'a valid request'
+        end
+      end
+
+      describe 'lowercase valid current_status' do
+        context 'current_status=up' do
+          let(:current_status) { 'up' }
+          it_behaves_like 'a valid request'
+        end
+
+        context 'current_status=down' do
+          let(:current_status) { 'down' }
+          it_behaves_like 'a valid request'
+        end
+      end
+
+      context 'valid current_status without a description' do
+        let(:current_status) { 'UP' }
+
+        it_behaves_like 'a valid request'
+
+        it 'does not create new StatusMessage' do
+          expect { post! }.not_to change { StatusMessage.count }
+        end
+      end
+
+      context 'valid current_status with description' do
+        let(:params) do
+          {
+            current_status: "UP",
+            description: "Describing a message here"
+          }
+        end
+
+        it_behaves_like 'a valid request'
+
+        it 'creates new StatusMessage' do
+          expect { post! }.to change {StatusMessage.count }.by(1)
         end
       end
     end
