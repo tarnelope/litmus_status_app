9d9161eb940a198aea1e8cb76f880ab696abeeaa
Teresa Tarn <teresa.tarn@kapost.com>
2018-09-11 11:30:29 -0400

    Added nil Status handler on front-end

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,12 @@ class StaticPagesController < ApplicationController
   private
 
   def current_status
-    @_current_status ||= Status.last
+    # TODO: Status.last returns nil if no status exist which wouldn't raise the exception. Either accept the changes where the status_pages_helper#status_class can handle current_status = nil or update to raise and handle exception.
+    begin
+      @_current_status = Status.last
+    rescue ActiveRecord::RecordNotFound
+      puts "Exception: Status not created"
+    end
   end
 
   def status_messages
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,7 +1,9 @@
 module StaticPagesHelper
   include Pagy::Frontend
-  
+
   def status_class(current_status)
+    return 'alert-warning' unless current_status
+
     current_status.current_status == 'UP' ? 'alert-success' : 'alert-danger'
   end
 end
